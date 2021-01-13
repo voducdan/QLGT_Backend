4412f916a94c3ad50c73d1024f068e7f3f8c0414
ptson21 <ngtranhtuan99@gmail.com>
2021-01-14 00:06:59 +0700

    update kh

---
+++ QLGT_API/Controllers/KhachHang_BienBangController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLGT_API.Repository;
+using QLGT_API.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLGT_API.Controllers
+{
+
+    [Route("api/KhachHang_BienBang")]
+    [ApiController]
+    public class KhachHang_BienBangController : ControllerBase
+    {
+        //private BienBangService bienBangService;
+        private BienBangRepository bienBangRepository;
+
+        public KhachHang_BienBangController(BienBangRepository bienBangRepository)
+        {
+            this.bienBangRepository = bienBangRepository;
+        }
+
+        [HttpGet("{id}")]
+        public ListBienBangKHView Get(int id)
+        {
+            ListBienBangKHView list = new ListBienBangKHView();
+            try
+            {
+                list.Data = bienBangRepository.GetAll(w => w.MA_KHACH_HANG == id);
+                list.MaKH = id;
+                
+            }
+            catch (IOException e)
+            {
+                list.Data = null;
+                list.MaKH = id;                
+            }
+            return list;
+        }
+    }
+}
+
---
+++ QLGT_API/Views/Repon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLGT_API.Views
+{
+    public class Repon
+    {
+        public int code { get; set; }
+        public string err { get; set; }
+    }
+}
