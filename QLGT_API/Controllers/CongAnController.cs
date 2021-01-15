a1ddc2c4916feef87301d0b472862da2be4823d7
ptson21 <ngtranhtuan99@gmail.com>
2021-01-15 15:34:21 +0700

    add getlist congan

---
+++ QLGT_API/Controllers/CongAnController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLGT_API.Model;
+using QLGT_API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLGT_API.Controllers
+{
+    [Route("api/congan")]
+    [ApiController]
+    public class CongAnController : ControllerBase
+    {
+        private CongAnRepository congAnRepository;
+        private CongAnService congAnService;
+
+        public CongAnController(CongAnRepository congAnRepository, CongAnService congAnService)
+        {
+            this.congAnRepository = congAnRepository;
+            this.congAnService = congAnService;
+        }
+        [HttpGet]
+        public List<CongAnModel> GetAll()
+        {
+            return congAnRepository.GetAll(m => m.HOAT_DONG == 1);
+        }
+    }
+}
---
+++ QLGT_API/Model/CongAnModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLGT_API.Model
+{
+    public class CongAnModel
+    {
+        [Key]
+        public int MA_SO_CONG_AN { get; set; }
+        public string TEN_CONG_AN { get; set; }
+       
+        public int HOAT_DONG { get; set; }
+    }
+}
